c2b5446c9413e805d31120a97b98565c0c205d03
Adam-smh <africa.adam1@gmail.com>
2021-06-25 16:16:06 +0200

    pray

--- scr1pts.js
+++ scr1pts.js
@@ -1,17 +1,32 @@
 let input = document.getElementById("input");
 
-function button(x) {
+function equal(x) {
   let inner = x.innerHTML;
 
   if (inner == "=") {
     input.innerHTML = eval(input.innerHTML);
-  } else if (inner == "C") {
-    input.innerHTML = "0";
   } else {
     if (input.innerHTML == "0") {
       input.innerHTML = inner;
     } else {
       input.innerHTML += inner;
     }
+  }
+}
+function clear(x) {
+  let inner = x.innerHTML;
+
+  if (inner == "C") {
+    input.innerHTML = "0";
+  }
+}
+
+function entries(x) {
+  let inner = x.innerHTML;
+
+  if (input.innerHTML == "0") {
+    input.innerHTML = inner;
+  } else {
+    input.innerHTML += inner;
   }
 }
